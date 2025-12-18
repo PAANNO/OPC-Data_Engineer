80fdd284c3cc4b667d45e1b0c7fb173e883985ba
Paul-Alexandre Annonay <pa.annonay@gmail.com>
2025-12-18 12:25:56 +0100

    Refactor cleaning functions: remove delete_empty_colums, enhance delete_high_missing_columns to handle empty numeric columns and improve logging.

--- oc_tools/cleaning.py
+++ oc_tools/cleaning.py
@@ -15,29 +15,23 @@ def delete_duplicated_rows(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def delete_empty_colums(df: pd.DataFrame) -> pd.DataFrame:
-    """Delete empty columns from a DataFrame."""
-    try:
-        empty_col = df.isnull().mean() == 1.0
-        if empty_col.sum() > 0:
-            df.drop(columns=df.columns[empty_col], inplace=True)
-        else:
-            print("No empty columns found.")
-    except Exception as e:
-        print(f"An error occurred while deleting empty columns: {e}")
-    return df
-
-
 def delete_high_missing_columns(
     df: pd.DataFrame, threshold: float = 0.3
 ) -> pd.DataFrame:
     """Delete columns with a high percentage of missing values from a DataFrame."""
     try:
-        high_missing_col = (df.isnull().mean() > threshold) & (df.dtypes != "float64")
-        if high_missing_col.sum() > 0:
-            df.drop(columns=df.columns[high_missing_col], inplace=True)
+        high_missing_col_obj = (df.isnull().mean() > threshold) & (
+            df.dtypes != "float64"
+        )
+        empty_num_col = (df.isnull().mean() == 1.0) & (df.dtypes == "float64")
+        if high_missing_col_obj.sum() > 0:
+            df.drop(columns=df.columns[high_missing_col_obj], inplace=True)
         else:
-            print("No columns with high missing values found.")
+            print(f"No high missing object columns found in {df}.")
+        if empty_num_col.sum() > 0:
+            df.drop(columns=df.columns[empty_num_col], inplace=True)
+        else:
+            print(f"No empty numeric columns found in {df}.")
     except Exception as e:
         print(f"An error occurred while deleting high missing columns: {e}")
     return df
@@ -46,6 +40,5 @@ def delete_high_missing_columns(
 def first_steps_cleaning(df: pd.DataFrame, missing_threshold: float) -> pd.DataFrame:
     """Perform first steps of cleaning on a DataFrame."""
     df = delete_duplicated_rows(df)
-    df = delete_empty_colums(df)
     df = delete_high_missing_columns(df, missing_threshold)
     return df
